2cb0e3da8ac2c2feec8ae8adabff569b071de683
NhungNguyen1408 <nhungnguyen.14082005@gmail.com>
2026-04-16 22:31:23 +0700

    feature login page

---
+++ src/services/auth.service.js
@@ -0,0 +1,20 @@
+import api from "./api";
+
+export const login = async (username, password) => {
+  const { data } = await api.post("/auth/login", { username, password });
+  localStorage.setItem("token", data.token);
+  localStorage.setItem("user", JSON.stringify(data.user));
+  return data;
+};
+
+export const logout = () => {
+  localStorage.removeItem("token");
+  localStorage.removeItem("user");
+};
+
+export const getToken = () => localStorage.getItem("token");
+
+export const getUser = () => {
+  const user = localStorage.getItem("user");
+  return user ? JSON.parse(user) : null;
+};
